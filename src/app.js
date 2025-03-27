a53ef9b17a5940cb9232a138dbce4dfb43625d14
Tonyreng <tonyreng01@gmail.com>
2025-03-27 02:43:27 +0000

    Generador de dominios terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our', 'your', 'my', 'their', 'his', 'her'];
+  let adj = ['great', 'big', 'Adorable', 'Aggressive', 'Athletic', 'Casual'];
+  let noun = ['jogger', 'racoon', 'cat', 'dog', 'rabbit', 'son'];
+  let domain = ['.com', '.net', '.us', '.io'];
+  let domainList = document.getElementById(domainlist);
+
+  
+
+  for (let i = 0; i < pronoun.length; i++ ) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainlist.innerHTML +=  `<li>${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}</li>`
+        }
+      }
+    }
+  }
+
+
+
+
+  
 };
